0ec810342b42253fe7ca17a3ace53b956d7efa44
Jamie Lincoln Ramsgaard <jami0092@stud.kea.dk>
2017-05-17 12:11:53 +0200

    Login

--- src/Servlet/Servlet.java
+++ src/Servlet/Servlet.java
@@ -1,11 +1,8 @@
 package Servlet;
 
-import AppLayer.User;
+import AppLayer.Login;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Created by Tolda on 16-05-2017.
@@ -26,12 +23,12 @@ public class Servlet extends javax.servlet.http.HttpServlet
     public void doPostLogin(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)throws javax.servlet.ServletException, IOException
 
     {
-        User userObject = new User();
+        Login login = new Login();
 
         request.setAttribute("username", request.getParameter("loginname"));
         request.setAttribute("password", request.getParameter("password"));
 
-        if (userObject.isValidUserCredentials(request.getParameter("loginname"), request.getParameter("password"))) {
+        if (login.isValidUserCredentials(request.getParameter("loginname"), request.getParameter("password"))) {
             request.getRequestDispatcher("/index.jsp").forward(request, response);
         } else{
             request.setAttribute("errorMessage", "Invalid login and password. Try again");
