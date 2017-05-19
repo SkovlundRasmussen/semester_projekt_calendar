326da9b72ea03bb784279bf959d8a3d857d6eaa2
Jamie Lincoln Ramsgaard <jami0092@stud.kea.dk>
2017-05-19 09:40:46 +0200

    Ny login

--- src/Controller/Login.java
+++ src/Controller/Login.java
@@ -1,12 +1,13 @@
-package AppLayer;
+package Controller;
 
 import DataLayer.LoginHandler;
+
 /**
- * Created by Jamie L. Ramsgaard on 5/16/2017.
+ * Created by Jamie L. Ramsgaard on 5/19/2017.
  */
 public class Login {
-    public boolean isValidUserCredentials(String sUserName, String sUserPassword){
-
+    public boolean isValidUserCredentials(String sUserName, String sUserPassword)
+    {
         LoginHandler LoginHandler_object = new LoginHandler();
 
         return LoginHandler_object.isValidUserLogin(sUserName,sUserPassword);
